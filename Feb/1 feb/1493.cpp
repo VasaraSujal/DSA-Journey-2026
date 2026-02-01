89980e4637384f53f8014c65571030f3302dd263
Sujal Vasara <vasarasujal.cg@gmail.com>
2026-02-01 19:33:19 +0530

    Solve leetcode question no 1493 using Sliding Window

---
+++ Feb/1 feb/1493.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int zeroCount = 0;
+        int maxLength = 0;
+        int i=0;
+        for(int j=0;j<nums.size();j++) {
+            if(nums[j] == 0) {
+                zeroCount++;
+            }
+            while(zeroCount > 1) {
+                if(nums[i] == 0) {
+                    zeroCount--;
+                }
+                i++;
+            }
+            maxLength = max(maxLength, j-i);
+        }
+        return maxLength;
+    }
+};
