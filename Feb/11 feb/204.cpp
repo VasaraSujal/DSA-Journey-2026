058d08d6dbd5be9fdd5a2a6f30e29550c5e2b169
Sujal Vasara <vasarasujal.cg@gmail.com>
2026-02-11 19:35:50 +0530

    Solved leetcode problem no 204 using Sieve of Eratosthenes algorithm

---
+++ Feb/11 feb/204.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        // Sieve of Eratosthenes :
+        vector<bool> isPrime(n+1, true);
+        for(int i = 2; i*i <= n; i++) {
+            if(isPrime[i] == true) {
+                for(int j = 2; i*j <= n; j++) {
+                    isPrime[i*j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for(int i = 2; i < n; i++) {
+            if(isPrime[i] == true) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+// Time Limit Exceeded
+
+//   Time Complexity : O(n*sqrt(n))
+
+// int count = 0;
+// for (int i = 2; i < n; i++) {
+//     bool isPrime = true;
+//     for (int j = 2; j * j <= i; j++) {
+//         if (i % j == 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//     if (isPrime) {
+//         count++;
+//     }
+// }
+// return count;
