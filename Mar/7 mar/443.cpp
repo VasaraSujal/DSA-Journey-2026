84004c9f732d0c1ad0f5496050f575729e4f422a
Sujal Vasara <vasarasujal.cg@gmail.com>
2026-03-07 23:28:35 +0530

    Solved leetcode question 443 from the leetcode 75 study plan

---
+++ Mar/7 mar/443.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n = chars.size();
+        int index = 0;
+
+        for(int i=0;i<n;) {
+            int ch = chars[i];
+            int count = 0;
+
+            while(i < n && chars[i] == ch) {
+                i++;
+                count++;
+            }
+
+            chars[index++] = ch;
+
+            if(count > 1) {
+                string s = to_string(count);
+                for(char c : s) {
+                    chars[index++] = c;
+                }
+            }
+        }
+        return index;
+    }
+};
