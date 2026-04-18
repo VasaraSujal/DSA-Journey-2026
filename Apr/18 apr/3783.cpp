8df93d1b255c5e3d0a1558322d032d6aa2362f66
Sujal Vasara <vasarasujal.cg@gmail.com>
2026-04-18 09:57:59 +0530

    Solved today's daily question

---
+++ Apr/18 apr/3783.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        int ori = n;
+        int rev = 0;
+        while(n > 0) {
+            int rem = n % 10;
+            rev = rev*10 + rem;
+            n /= 10;
+        }
+        return abs(ori - rev);
+    }
+};
