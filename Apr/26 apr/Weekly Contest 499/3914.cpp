4a8fa20d68c60b8a3a805ad7c7ff6b35f5109f81
Sujal Vasara <vasarasujal.cg@gmail.com>
2026-04-26 14:55:36 +0530

    Solved third question of contest

---
+++ Apr/26 apr/Weekly Contest 499/3914.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long minOperations(vector<int>& nums) {
+        long long ans = 0;
+        for(int i=1;i<nums.size();i++) {
+            if(nums[i] < nums[i-1]) {
+                ans += 1LL * (nums[i-1] - nums[i]);
+            }
+        }
+        return ans;
+    }
+};
